5ebb4b4fd40a7b6d956e54652a005422831211d5
mike corcoran <corcoran909@gmail.com>
2025-08-31 16:27:17 +0100

    added registration

---
+++ app/src/components/register/register.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'; // ✅ Import this
+import './register.css';
+
+export default function Regiser() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('')
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('')
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (password1 != password2) {
+            alert("Passwords do not match")
+        }
+        else {
+            try {
+                const response = await axios.post('/api/register', { username, email, password }, {
+                    withCredentials: true
+                });
+                navigate('/login');
+
+            } catch (err) {
+                console.error("Registration error:", err); // Full object
+                console.log("Error response:", err?.response); // Optional chaining
+                if (err.response && err.response.status === 409)
+                    alert("email or username already taken");
+                else alert("Something went wrong")
+            }
+        }
+    };
+
+    return (
+        <div className="register-container">
+            <form onSubmit={handleSubmit} className="register-form">
+                <h2>Enter Details to Register</h2>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input
+                    type="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password1}
+                    onChange={e => setPassword1(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={password2}
+                    onChange={e => setPassword2(e.target.value)}
+                />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+}
